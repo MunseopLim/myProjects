debea0c5c9d8da25330f82f3f41e27d55278b65e
munseop.lim <munseop.lim@samsung.com>
2024-12-18 20:05:57 -0800

    under construction 화면 구성

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: process.env.METADATA_TITLE,
-  description: process.env.METADATA_DESCRIPTION,
+  title: process.env.NEXT_PUBLIC_METADATA_TITLE,
+  description: process.env.NEXT_PUBLIC_METADATA_DESCRIPTION,
 };
 
 export default function RootLayout({
--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,9 +1,33 @@
+'use client'
+
 import "bootstrap/dist/css/bootstrap.min.css";
+import Typewriter from 'typewriter-effect';
+import { Navbar, Nav } from 'react-bootstrap';
 
 export default function Home() {
   return (
-    <div>
-      Hello World
-    </div>
+    <>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">{process.env.NEXT_PUBLIC_METADATA_TITLE}</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="https://specialguy.tistory.com/">Blog</Nav.Link>
+            <Nav.Link href="https://github.com/MunseopLim/">GitHub</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <div style={{ height: '400px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <h1>
+          <Typewriter
+            options={{
+              strings: ['Under Construction'],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </h1>
+      </div>
+    </>
   );
 }
